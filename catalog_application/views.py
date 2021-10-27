70fac953375799d2cfd28524fc8e23357abb1192
Ronnie <rmmukai@gmail.com>
2021-10-27 14:52:55 -0700

    10/27/2021 - Created desktop_link.html - Started the template that all html pages will use - Created desktop_links.css - Changed desktop_link url from index to desktop_links

--- catalog_application/views.py
+++ catalog_application/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, redirect, HttpResponse
 from .models import *
 from django.contrib import messages
 
-def index(request):
-    return HttpResponse("Hello")
+def desktop_links(request):
+    return render(request, 'desktop_links.html')
 
