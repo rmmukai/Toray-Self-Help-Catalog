54de5de2b864e31c05ef4bf8047d39767bb27466
Ronnie <rmmukai@gmail.com>
2021-11-01 16:01:47 -0700

    11/01/2021 - Created home_page.html - Created admin_page.html - Created home_page.css - Created admin_page.css - Started defs in views.py for the above html pages

--- catalog_application/urls.py
+++ catalog_application/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.desktop_links),
+    path('', views.home_page),
+    path('admin_page', views.admin_page),
+    path('desktop_links', views.desktop_links),
 ]
--- catalog_application/views.py
+++ catalog_application/views.py
@@ -5,3 +5,8 @@ from django.contrib import messages
 def desktop_links(request):
     return render(request, 'desktop_links.html')
 
+def admin_page(request):
+    return render(request, 'admin_page.html')
+
+def home_page(request):
+    return render(request, 'home_page.html')
