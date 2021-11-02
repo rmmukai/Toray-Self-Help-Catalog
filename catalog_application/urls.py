0964465e1c46d0664c2eb6cb57c1da680538f9fa
Ronnie <rmmukai@gmail.com>
2021-11-02 15:02:49 -0700

    11/2/2021 - Re-created both add_self_help_article.html and css. 	Issues with images. The images are temporary will troubleshoot when I get final images. - Created def in views.py for adding a self help article. 	- See def create_article.

--- catalog_application/urls.py
+++ catalog_application/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page),
+    path('desktop_links', views.desktop_links),
     path('admin_page', views.admin_page),
     path('admin_page/add_self_help_article', views.add_self_help_article),
-    path('desktop_links', views.desktop_links),
 ]
+
--- catalog_application/views.py
+++ catalog_application/views.py
@@ -2,15 +2,36 @@ from django.shortcuts import render, redirect, HttpResponse
 from .models import *
 from django.contrib import messages
 
+
 def desktop_links(request):
     return render(request, 'desktop_links.html')
+
 
 def admin_page(request):
     return render(request, 'admin_page.html')
 
+
 def home_page(request):
     return render(request, 'home_page.html')
 
+
 def add_self_help_article(request):
     return render(request, 'add_self_help_article.html')
+
+
+def create_article(request):
+    # Error/validator messages found in models.py.
+    errors = SelfHelpArticle.objects.self_help_article_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/admin_page/add_self_help_article')
+    else:
+        new_self_help_articles = SelfHelpArticle.objects.create(
+            title=request.POST['title'],
+            created_by=request.POST['created_by'],
+            description=request.POST['description'],
+            document_location=request.POST['document_location'],
+        )
+        return redirect('/admin_page')
 
