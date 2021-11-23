1ddeb71a6980f1eec167f891954b3f5ab51ecfa2
Ronnie <rmmukai@gmail.com>
2021-11-23 09:39:34 -0800

    11/23/2021: - Continued testing adding and editing articles - Started the delete popup window creation and functionality 	- Finished HTML portion (still need to test when other elements are done) 	- Finished css portion (still need to test when other elements are done) 	- Started js potion 	- Still need to do the Python/Django portion

---
+++ static/javascript/all_self_help_articles.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const deleteWindow = document.querySelector("delete_window");
+const overlay = document.querySelector("overlay");
+// This "link" is blank, not connecting to anywhere
+const deleteLink = document.querySelector("delete_link");
+
+const deleteWindowShow = 
