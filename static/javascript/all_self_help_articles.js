6093e0b050f51f7e27a8a69b91cc1e9727cfe7a4
Ronnie <rmmukai@gmail.com>
2021-11-24 15:12:40 -0800

    11/24/2021: - Troubleshot error validator appearing on add_self_help_article.html on page load. Fixed. - Continued working on article deletion. Thinking it might be better to put the delete button on the edit_self_help_article.html page. 	- Getting js function error for the onClick delete window. Need to troubleshoot further.

--- static/javascript/all_self_help_articles.js
+++ static/javascript/all_self_help_articles.js
@@ -1,8 +1,34 @@
 "use strict";
 
-const deleteWindow = document.querySelector("delete_window");
-const overlay = document.querySelector("overlay");
+// delete_link class related
 // This "link" is blank, not connecting to anywhere
-const deleteLink = document.querySelector("delete_link");
+const deleteLink = document.querySelectorAll("delete_link");
 
-const deleteWindowShow = 
+// overlay class related
+const overlay = document.querySelector("overlay");
+
+const overlayOn = function () {
+    overlayclassList.remove("hidden");
+};
+
+const overlayOff = function () {
+    overlay.classList.remove("hidden");
+};
+
+// delete_window class related
+const deleteWindow = document.querySelectorAll("delete_window");
+
+const deleteWindowOn = function () {
+    deleteWindow.classList.remove("hidden");
+};
+
+const deleteWindowoff = function () {
+    deleteWindow.classList.add("hidden");
+};
+
+for (let i = 0; i < deleteLink.length; i++) {
+    deleteLink[i].addEventListener("click", function () {
+        deleteWindow.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    });
+}
